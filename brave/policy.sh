bf2d5edd7ee9a69736251a107dd9b2aec1f0bc1f
somni <82272900+somnisomni@users.noreply.github.com>
2025-12-23 23:09:48 +0900

    Create group policy setting script for Brave browser

---
+++ brave/policy.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+###########################
+# somni.dotfiles :: macOS #
+###########################
+### This script will disable some nonsense (for me) features such as AI, Web3, etc.
+### Supports stable, beta, nightly channel installations.
+
+# Declare Brave package name list
+declare -a packageNames=(
+    "com.brave.Browser"
+    "com.brave.Browser.beta"
+    "com.brave.Browser.nightly"
+)
+
+# Declare group policy settings pair
+declare -A policySettings=(
+    ["BraveRewardsDisabled"]=true
+    ["BraveWalletDisabled"]=true
+    ["BraveVPNDisabled"]=true
+    ["BraveAIChatEnabled"]=false
+    ["BraveNewsDisabled"]=true
+    ["BraveTalkDisabled"]=true
+)
+
+# Set group policies for all available installations
+for name in ${packageNames[@]}; do
+    if defaults read "$name" &> /dev/null; then
+        echo "Setting group policies for $name..."
+
+        for policy value in ${(@kv)policySettings}; do
+            defaults write "$name" "$policy" -bool $value
+        done
+
+        echo "Done."
+    else
+        echo "Seems like $name is not installed, skipping..."
+    fi
+
+    echo ""
+done
